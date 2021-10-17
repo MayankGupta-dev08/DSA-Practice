b86d81cd7e6e1d7a44c866b0dd035d01c5423c54
Mayank Gupta <ce.mayank8@gmail.com>
2021-10-17 19:28:18 +0530

    done level1 questions of recurrsion + ArrayList

---
+++ c117_recurrsion_AL_printAllSubsequences.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class c117_recurrsion_AL_printAllSubsequences {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        sc.close();
+        ArrayList<String> ans = allSubsequences_recurrsion(str);
+        System.out.println(ans);
+    }
+
+    // total number of subsequence = 2^n and total substrings = (n(n+1))/2
+    // faith will be of bc --> [--, -c, b-, bc]
+    // our expectation for abc --> [---, --c, -b-, -bc, a--, a-c, ab-, abc]
+    public static ArrayList<String> allSubsequences_recurrsion(String str) {
+        if (str.length() == 0) {
+            ArrayList<String> baseRes = new ArrayList<>();
+            baseRes.add("");
+            return baseRes;
+        }
+
+        char ch = str.charAt(0);
+        String ros = str.substring(1);
+        ArrayList<String> recurrRes = allSubsequences_recurrsion(ros);
+
+        ArrayList<String> myRes = new ArrayList<>();
+        for (String recrStr : recurrRes) {
+            myRes.add("" + recrStr);
+            myRes.add(ch + recrStr);
+        }
+        return myRes;
+    }
+}
