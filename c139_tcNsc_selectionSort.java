ed9172fa179e292cda12ef1d8d3a24f18e493bb9
Mayank Gupta <ce.mayank8@gmail.com>
2021-10-22 15:45:00 +0530

    done 5 level1 time and space complexity questions --> Sorting

---
+++ c139_tcNsc_selectionSort.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class c139_tcNsc_selectionSort {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+
+        selectionSort(arr);
+        printArray(arr);
+    }
+
+    // firstly assuming ith idx is min and then for ith idx finding minIdx using
+    // inner loop and then at the end of inner loop swapping ith idx with minIdx.
+    // If n-1 smallest elements are sorted, then whole array of size n is sorted, so
+    // total outer iterations --> n-1
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
+
+// input
+// 5
+// 7 -2 4 1 3
+
+// output
+// -2 1 3 4 7
---
+++ c140_tcNsc_insertionSort.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class c140_tcNsc_insertionSort {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scn.nextInt();
+        }
+        scn.close();
+
+        insertionSort(arr);
+        printArray(arr);
+    }
+
+    // inserting ith idx element in its right place in the part of the array before
+    // it, in this also n-1 outer iterations (start from i=1 as want to compare it with i=0).
+    // we will pause the inner iteration for an ith value as soon as the current jth
+    // idx element is found to be greater that its prev element is the array.
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+}
+
+// input
+// 5
+// 7 -2 4 1 3
+
+// output
+// -2 1 3 4 7
