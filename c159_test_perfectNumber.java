2bb5064a1d74574004592f210af735420d7546b1
Mayank Gupta <ce.mayank8@gmail.com>
2021-11-02 23:16:06 +0530

    solved test questions

---
+++ c159_test_perfectNumber.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class c159_test_perfectNumber {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        if (isPerfectNumber(n)) {
+            System.out.println("Perfect");
+        } else {
+            System.out.println("Not Perfect");
+        }
+    }
+
+    public static boolean isPerfectNumber(int n) {
+        int sum = 0;
+        for (int div = 1; div < n; div++) {
+            if (n % div == 0) {
+                sum += div;
+            }
+        }
+
+        if (sum == n) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
