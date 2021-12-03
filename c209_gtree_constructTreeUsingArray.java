8dda3f3bde5c10d5f950a4bf1a0afdd9e15ff016
Mayank Gupta <ce.mayank8@gmail.com>
2021-12-04 00:58:51 +0530

    started level1 generic trees

---
+++ c209_gtree_constructTreeUsingArray.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class c209_gtree_constructTreeUsingArray {
+
+    public static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<Node>();
+    }
+
+    public static Node constructGenericTree(int[] arr) {
+        Node root = null;
+        Stack<Node> st = new Stack<Node>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                st.pop();
+            } else {
+                Node temp = new Node();
+                temp.data = arr[i];
+
+                if (st.size() == 0) {
+                    root = temp;
+                } else {
+                    st.peek().children.add(temp);
+                }
+
+                st.push(temp);
+            }
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100, -1, -1,
+                -1 };
+
+        constructGenericTree(arr);
+    }
+}
