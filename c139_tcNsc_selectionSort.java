a90cf8b947e0bdcbd2088ee96c3336c72dca0725
Mayank Gupta <ce.mayank8@gmail.com>
2021-12-14 15:13:54 +0530

    min swaps

--- c139_tcNsc_selectionSort.java
+++ c139_tcNsc_selectionSort.java
@@ -36,6 +36,8 @@ public class c139_tcNsc_selectionSort {
         }
     }
 
+    // for selection sort - minimum swaps = n-1
+
     public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
