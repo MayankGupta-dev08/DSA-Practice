855fb5e680172a5b499ae4f3bd88b6a6bf05f26c
Mayank Gupta <ce.mayank8@gmail.com>
2022-07-15 00:44:19 +0530

    revised 2 more questions --> till q30

--- c1-c99/c29_pattern10.java
+++ c1-c99/c29_pattern10.java
@@ -8,7 +8,7 @@ public class c29_pattern10 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i+j==n-1)
+                if (i + j == n - 1)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
@@ -19,12 +19,12 @@ public class c29_pattern10 {
 }
 
 /*
-5
-
+ * 5
+ 
         *
       *
     *   
   *
 *
 
-*/
+ */
--- c1-c99/c30_pattern11.java
+++ c1-c99/c30_pattern11.java
@@ -8,7 +8,7 @@ public class c30_pattern11 {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if(i+j==n-1 || i==j)
+                if (i + j == n - 1 || i == j)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
@@ -26,6 +26,5 @@ public class c30_pattern11 {
     *
   *   *
 *       *
-
-*/
-
+ 
+ */
