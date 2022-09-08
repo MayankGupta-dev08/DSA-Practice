a9d4d75c85102aeb9bcd233bb08b32d2a6c82ae6
Mayank Gupta <ce.mayank8@gmail.com>
2022-09-09 00:31:40 +0530

    isPrime question - improved solution added

--- c1-c99/c1-c9/c8_isPrime_v2.java
+++ c1-c99/c1-c9/c8_isPrime_v2.java
@@ -15,6 +15,9 @@ public class c8_isPrime_v2 {
         scn.close();
     }
 
+
+    // The algorithm can be improved further by observing that all primes are of the form 6k ± 1, with the exception of 2 and 3. This is because all integers can be expressed as (6k + i) for some integer k and for i = ?1, 0, 1, 2, 3, or 4; 2 divides (6k + 0), (6k + 2), (6k + 4); and 3 divides (6k + 3). So a more efficient method is to test if n is divisible by 2 or 3, then to check through all the numbers of form 6k ± 1. (Source: wikipedia)
+
     public static boolean isPrime(int n) {
         // corner cases
         if (n <= 1)
