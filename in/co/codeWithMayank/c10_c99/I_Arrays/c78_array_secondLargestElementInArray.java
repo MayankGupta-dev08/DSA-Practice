7ed7a9feb0e7445f4b699bfcded37618c5580425
Mayank Gupta <ce.mayank8@gmail.com>
2023-09-19 01:12:13 +0530

    refactored the code

--- in/co/codeWithMayank/c10_c99/I_Arrays/c78_array_secondLargestElementInArray.java
+++ in/co/codeWithMayank/c10_c99/I_Arrays/c78_array_secondLargestElementInArray.java
@@ -1,11 +1,12 @@
 package in.co.codeWithMayank.c10_c99.I_Arrays;
 
-import java.util.*;
+import java.util.Scanner;
+
 public class c78_array_secondLargestElementInArray {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
-        
+
         int[] arr = new int[n];
         for (int i = 0; i < arr.length; i++) {
             arr[i] = scn.nextInt();
@@ -17,30 +18,29 @@ public class c78_array_secondLargestElementInArray {
     }
 
     // For this time complexity is O(n)
-        // easier would have been sorting the array and returning the first element from the last which is not equal to arr[arr.length-1]. for this time complexity = O(nlogn) would have been worse.
+    // easier would have been sorting the array and returning the first element from the last which is not equal to arr[arr.length-1].
+    // for this time complexity = O(nlogn) would have been worse.
     public static int get2ndLargestElement_brute(int[] arr) {
-        int first=arr[0], second = Integer.MIN_VALUE;
+        int first = arr[0], second = Integer.MIN_VALUE;
         for (int i = 1; i < arr.length; i++) {
             first = Math.max(first, arr[i]);
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i]!=first){
-                second = Math.max(second, arr[i]);
-            }
+        for (int j : arr) {
+            if (j != first)
+                second = Math.max(second, j);
         }
         return second;
     }
 
     // Better approach, only one traversal
     public static int get2ndLargestElement_optimal(int[] arr) {
-        int first=arr[0], second = Integer.MIN_VALUE;
+        int first = arr[0], second = Integer.MIN_VALUE;
         for (int i = 1; i < arr.length; i++) {
-            if(arr[i]>first){
+            if (arr[i] > first) {
                 second = first;
                 first = arr[i];
-            }
-            else if(arr[i]<first && arr[i]>second){
+            } else if (arr[i] < first && arr[i] > second) {
                 second = arr[i];
             }
         }
